3d0fc53159ed490ce6d5e892e4e03a90aae72888
DhanushM12 <dhanushmukhi@gmail.com>
2022-08-12 20:10:09 +0530

    added user flow

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const io = require('socket.io')(3000, {
 })
 
 const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name)
+    })
+})
--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const socket = io('http://localhost:3000')
 
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+
+let name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message; //<div>message<div/>
+    messageContainer.append(messageElement);
+}
