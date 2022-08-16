9a61c3f20bf1bacd5852c1a952894d09a564f6a0
DhanushM12 <dhanushmukhi@gmail.com>
2022-08-16 19:51:23 +0530

    added chat

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ socket.on('user-connected', name => {
     appendMessage(`${name} connected`)
 })
 
+socket.on('chat-message', data => {
+    appendMessage(`${data.name} : ${data.message}`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    if(message !== ''){
+        appendMessage(`You: ${message}`);
+        socket.emit('send-chat-message', message)
+    }
+    messageInput.value = '';
+})
+
+
 function appendMessage(message){
     const messageElement = document.createElement('div');
     messageElement.innerText = message; //<div>message<div/>
